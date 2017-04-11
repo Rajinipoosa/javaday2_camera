3265b7f2ca077d375e137400166ecadbac699ee5
Rajini <rajinipoosa@gmail.com>
2017-04-11 18:44:07 +0100

    added analogcamera and digital camera classes

--- PhotographerTest.java
+++ PhotographerTest.java
@@ -3,27 +3,34 @@ import org.junit.*;
  
  public class PhotographerTest{
   Photographer photographer;
-  Printable cameras;
+  AnalogCamera analog;
+  DigitalCamera digital;
+
 
   @Before
   public void before(){
     photographer = new Photographer();
-    cameras = new Printable();
-   
-
-   }
+    analog = new AnalogCamera();
+    digital = new DigitalCamera();
+    }
    @Test
    public void addOneToCamerasList()
    {
-    
-    assertEquals(1,photographer.addCamera(cameras));
+    photographer.addCamera(analog);
+    assertEquals(2,photographer.addCamera(digital));
    
    }
    @Test
-   public void removeOneFromCamerasList()
-   {
+   public void removeOneFromCamerasList(){
+    photographer.addCamera(analog);
+    photographer.addCamera(digital);
     
-    assertEquals(0,photographer.removeCamera(cameras));
+    assertEquals(1,photographer.removeCamera(analog));
    
+   }
+   @Test
+   public void displayAllCameraDetails(){
+    
+
    }
 }
