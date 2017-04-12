a3170d0386d0da763653eec8e6bc3dd32cd4a020
Rajini <rajinipoosa@gmail.com>
2017-04-12 08:56:32 +0100

    added all files but did not work on hashmaps

--- Photographer.java
+++ Photographer.java
@@ -1,39 +1,41 @@
 import java.util.*;
-import java.util.HashMap;
+ import java.util.HashMap;
 
 public class Photographer{
  public static void main(String[] args){
-  HashMap<String, Integer> journal = new HashMap<String,Integer>();
+  HashMap<String, Integer> 
+  journal = new HashMap<String,Integer>();
+  // return System.out.println("hello");
+
   journal.put("day1",5);
   journal.put("day2",10);
-}
+  // System.out.println(journal.size());
+    }
 private ArrayList<Printable> cameras;
-
-
+// }
 public Photographer(){
   this.cameras = new ArrayList<Printable>();
-}
+  // this.journal = new HashMap<String,Integer>();
+   }
 public int addCamera(Printable camera){
   cameras.add(camera);
   return cameras.size();
-}
+  }
 public int removeCamera(Printable camera){
   cameras.remove(camera);
   return cameras.size();
-}
+ }
 public String cameraDetails(){
   String details = "";
   for(Printable camera : this.cameras){
    details = details +""+ camera.printDetails()  ;
-  }
+    }
   return details;
-}
+ }
 public int numberOfPhotos(){
-  int count = 0;
-  for(Printable items : journal.values()){
-    add(items);
-    
-  }
+   int count = 0;
+  
+  System.out.println(cameras.journal.get("day1"));
   }
 
 
